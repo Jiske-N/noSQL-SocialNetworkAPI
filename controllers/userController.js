3bcdbc51733f5807ca0adaa797c8d9158b9e6fdf
@r0f33 <jiske.nicolson@gmail.com>
2024-06-25 21:32:28 +0930

    controllers/userController add friend by user and friend id route and routes/api/userRoutes reflects new route

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,4 +93,28 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    // Add a new friend to users friend list
+    async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {
+                    _id: req.params.userId,
+                },
+                { $addToSet: { friends: req.params.friendId } },
+                // make the updated user return rather than the original
+                { new: true }
+            );
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: "No user with that ID" });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 };
