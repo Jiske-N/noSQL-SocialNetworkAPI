cde344b083afcdbad9e5f7255ccc5cf9a7789a4f
@r0f33 <jiske.nicolson@gmail.com>
2024-06-25 15:01:52 +0930

    controllers/userController update user by id route and routes/api/userRoutes reflects new route

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,7 +30,9 @@ module.exports = {
         try {
             const user = await User.findOne({
                 _id: req.params.userId,
-            }).populate(["thoughts", "friends"]);
+            });
+            // not sure if it's necessary
+            // .populate(["thoughts", "friends"]);
 
             if (!user) {
                 return res
@@ -48,6 +50,30 @@ module.exports = {
     async createUser(req, res) {
         try {
             const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Update a single user by Id
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {
+                    _id: req.params.userId,
+                },
+                { $set: req.body },
+                // make the updated user return rather than the original
+                { new: true }
+            );
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: "No user with that ID" });
+            }
+
             res.json(user);
         } catch (err) {
             res.status(500).json(err);
